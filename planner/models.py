f11c090946e4921e8961ead93100c8172a6fd777
Armin Mozafary <armin.arm90@gmail.com>
2024-09-01 08:11:08 +0330

    creating models

--- planner/models.py
+++ planner/models.py
@@ -1,1 +1,30 @@
 from django.db import models
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=250)
+    description = models.TextField(null=True, blank=True)
+    duration_minutes = models.PositiveIntegerField()  # Duration in minutes
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Day(models.Model):
+    date = models.DateField()
+    tasks = models.ManyToManyField(Task)
+
+    def total_hours(self):
+        total_minutes = sum(task.duration_minutes for task in self.tasks.all())
+        return total_minutes / 60
+
+
+class Week(models.Model):
+    start_date = models.DateField()
+    end_date = models.DateField()
+    days = models.ManyToManyField(Day)
+
+    def total_hours(self):
+        total_minutes = sum(day.total_hours() * 60 for day in self.days.all())
+        return total_minutes / 60
