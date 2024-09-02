5b709efa01982774f4cecac0b1c98f140ef94a76
Armin Mozafary <armin.arm90@gmail.com>
2024-09-02 09:19:17 +0330

    customize user model

--- account/manager.py
+++ account/manager.py
@@ -2,7 +2,7 @@ from django.contrib.auth.base_user import BaseUserManager
 class UserManager(BaseUserManager):
     use_in_migrations = True
  
-    def create(self, phone, password=None, **extra_fields):
+    def create_user(self, phone, password=None, **extra_fields):
         if not phone:
             raise ValueError('Phone number is required')
         user = self.model(phone=phone, **extra_fields)
--- account/views.py
+++ account/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from .models import User
+from rest_framework import generics, permissions
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from .serialisers import UserSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-# Create your views here.
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = UserSerializer
+
+class LoginView(TokenObtainPairView):
+    permission_classes = (permissions.AllowAny,)
+
+class LogoutView(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
