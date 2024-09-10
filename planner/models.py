de131ffccc17ba7fbe283b8aa8eda4321f806906
Armin Mozafary <armin.arm90@gmail.com>
2024-09-10 10:58:54 +0330

    add user to planner model

--- planner/models.py
+++ planner/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from account.models import User
 
+
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=250)
@@ -15,17 +16,25 @@ class Task(models.Model):
 class Day(models.Model):
     date = models.DateField()
     tasks = models.ManyToManyField(Task)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def total_hours(self):
         total_minutes = sum(task.duration_minutes for task in self.tasks.all())
         return total_minutes / 60
+
+    def __str__(self):
+        return f"{self.date}"
 
 
 class Week(models.Model):
     start_date = models.DateField()
     end_date = models.DateField()
     days = models.ManyToManyField(Day)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def total_hours(self):
         total_minutes = sum(day.total_hours() * 60 for day in self.days.all())
         return total_minutes / 60
+
+    def __str__(self):
+        return f"start : {self.start_date}, End : {self.end_date}"
